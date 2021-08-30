87d9739767ce63493219e6474cfc7ed9d50529d6
saknet <andreas.andra@helsinki.fi>
2021-08-30 14:18:03 +0300

    improved time selection

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ require( 'cesium/Widgets/widgets.css' );
 
 const Cesium = require('cesium/Cesium');
 const featurePickerService = require( './services/featurepicker' );
+const confirmTimesButton = document.getElementById( "confirm-times" );
 var startDate = new Date( Date.now() - 14400000 );
 var endDate = new Date( Date.now() );
+var hours = 4;
+
+confirmTimesButton.addEventListener("click", confirmTimes);
 
 var viewer = new Cesium.Viewer( 'cesiumContainer' );
 var tileset = viewer.scene.primitives.add( new Cesium.Cesium3DTileset( {
@@ -23,23 +27,30 @@ window.onload = function() {
 
     hoursselect.onchange = function() {
 
-        let clockGD = Cesium.JulianDate.toGregorianDate( viewer.clock.currentTime );
-        let hours = hoursselect.value;
-        let newEndDate = new Date( clockGD.year, clockGD.month - 1, clockGD.day, clockGD.hour + 3, clockGD.minute, clockGD.second, clockGD.millisecond );
+        hours = hoursselect.value;
+      
+    } 
+}
+
+/* Function that confirms time selection and restarts feature picker service */
+function confirmTimes() {
+
+    let clockGD = Cesium.JulianDate.toGregorianDate( viewer.clock.currentTime );
+    let newEndDate = new Date( clockGD.year, clockGD.month - 1, clockGD.day, clockGD.hour + 3, clockGD.minute, clockGD.second, clockGD.millisecond );
+
+    if ( new Date( endDate ).getTime() >= new Date( newEndDate ).getTime() ) {
         
-        if ( new Date( endDate ).getTime() >= new Date( newEndDate ).getTime() ) {
-        
-            endDate = newEndDate.toISOString();
-            startDate = new Date( newEndDate - 3600000 * hours ).toISOString();
+        endDate = newEndDate.toISOString();
+        startDate = new Date( newEndDate - 3600000 * hours ).toISOString();
+
+    } else {
     
-        } else {
-        
-            startDate = new Date( Date.now() - 3600000 * hours  ).toISOString();
-            endDate = new Date( Date.now() ).toISOString();
-    
-        }
-        
-        Cesium.when( tileset.readyPromise ).then( function ( tileset ) { viewer.flyTo( tileset ) } ).then( featurePickerService.active3DTilePicker( viewer, startDate, endDate ) );
+        startDate = new Date( Date.now() - 3600000 * hours  ).toISOString();
+        endDate = new Date( Date.now() ).toISOString();
+        alert("Selected time can't be in the future");
 
     } 
+
+    Cesium.when( tileset.readyPromise ).then( function ( tileset ) { viewer.flyTo( tileset ) } ).then( featurePickerService.active3DTilePicker( viewer, startDate, endDate ) );
+
 }
