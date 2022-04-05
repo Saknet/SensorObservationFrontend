e1374f53cea17d14adc3f13945b923e17223ed45
saknet <andreas.andra@helsinki.fi>
2022-04-05 21:21:02 +0300

    cleared index.js

--- src/index.js
+++ src/index.js
@@ -7,19 +7,7 @@ const cesiumviewerService = require( './services/cesiumviewer' );
 const districtService = require( './services/district' );
 const datepickerService = require( './services/datepicker' );
 
-/**
- * Calls functions used for initialization
- */
-function init () {
-
-    let viewer = cesiumviewerService.initializeCesium();
-    datepickerService.initializeDatepicker();
-    districtService.initializeDistricts( viewer );
-    geocodingService.initializeGeocoding( viewer );
-}
-
-window.onload = function () {
-
-    init();
-    
-};
+let viewer = cesiumviewerService.initializeCesium();
+datepickerService.initializeDatepicker();
+districtService.initializeDistricts( viewer );
+geocodingService.initializeGeocoding( viewer );
